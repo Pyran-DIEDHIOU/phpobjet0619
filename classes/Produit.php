a7a12198a51a6b6c8dd275f3e62bdab733db38bb
pyran <diedhiou.pyrano@gmail.com>
2019-06-03 10:52:28 +0200

    premier commit

---
+++ classes/Produit.php
@@ -0,0 +1,11 @@
+<?php
+class Produit {
+    public $id;
+    public $name;
+    public $description;
+    public $price;
+    public $nbVues;
+    public $dateCreation;
+    public $dateModification;
+    public $etatPublication;
+}
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php require 'inc/header.php'; ?>
+
+<?php
+// On inclue la classe pour l'utiliser
+require 'Classes/Produit.php';
+// On crée un nouveau Produit
+$hamac = new Produit();
+$hamac->name = "Hamac";
+var_dump($hamac);
+// On créer un 2ème objet
+$parasol = new Produit();
+$parasol->name = "Parasol";
+var_dump($parasol);
+?>
+
+
+
+    <h1>Liste des produits</h1>
+
+<?php require 'inc/footer.php'; ?>
